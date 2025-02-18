5e7ef85c556dff8e4f1f678fa40d782d8f4a0f7c
EC2 Default User <ec2-user@ip-172-31-84-18.ec2.internal>
2025-02-18 02:06:57 +0000

    my first CDK project

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,16 +1,16 @@
-from aws_cdk import (
-    # Duration,
-    Stack,
-    # aws_sqs as sqs,
-)
-from constructs import Construct
+import aws_cdk as cdk
+import aws_cdk.aws_s3 as s3
 
-class HelloCdkStack(Stack):
+class HelloCdkStack(cdk.Stack):
 
-    def __init__(self, scope: Construct, construct_id: str, **kwargs) -> None:
+    def __init__(self, scope: cdk.App, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
         # The code that defines your stack goes here
+        bucket = s3.Bucket(self,"MyFirstBucketViaCDK",
+                           versioned=True,
+                           removal_policy=cdk.RemovalPolicy.DESTROY,
+                           auto_delete_objects=True)
 
         # example resource
         # queue = sqs.Queue(
