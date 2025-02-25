8ef5a222bf7cf4e2a9d2ab2f63e205dbb82d3a06
EC2 Default User <ec2-user@ip-172-31-84-18.ec2.internal>
2025-02-25 00:51:11 +0000

    static website hosting using S3

--- hello_cdk/hello_cdk_stack.py
+++ hello_cdk/hello_cdk_stack.py
@@ -1,5 +1,6 @@
 import aws_cdk as cdk
 import aws_cdk.aws_s3 as s3
+import aws_cdk.aws_s3_deployment as s3deployment
 
 class HelloCdkStack(cdk.Stack):
 
@@ -8,10 +9,16 @@ class HelloCdkStack(cdk.Stack):
 
         # The code that defines your stack goes here
         bucket = s3.Bucket(self,"MyFirstBucketViaCDK",
+                           block_public_access=s3.BlockPublicAccess.BLOCK_ACLS,
+                           access_control=s3.BucketAccessControl.BUCKET_OWNER_FULL_CONTROL,
+                           public_read_access=True,
+                           website_index_document="index.html",
                            versioned=True,
                            removal_policy=cdk.RemovalPolicy.DESTROY,
                            auto_delete_objects=True)
-
+        deployment = s3deployment.BucketDeployment(self,"DeployWebsite",
+                                sources=[s3deployment.Source.asset('./hello_cdk/static-website.zip')],
+                                destination_bucket=bucket)
         # example resource
         # queue = sqs.Queue(
         #     self, "HelloCdkQueue",
